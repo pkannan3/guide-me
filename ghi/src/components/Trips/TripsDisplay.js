09a3ace70ac851d1a8e28a66a83e35435b6e16d1
Priyanka Kannan <priyankannan91@gmail.com>
2024-02-01 17:58:45 -0600

    .modal Width Adjusted, 'Trip Name:' Spelling Corrected

--- ghi/src/components/Trips/TripsDisplay.js
+++ ghi/src/components/Trips/TripsDisplay.js
@@ -309,16 +309,11 @@ function TripsList(props) {
                     </button>
                     <div className="header"> Create Trip </div>
                     <div className="content">
-                      {" "}
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                      Atque, a nostrum. Dolorem, repellat quidem ut, minima sint
-                      vel eveniet quibusdam voluptates delectus doloremque,
-                      explicabo tempore dicta adipisci fugit amet dignissimos?
+                      Trip Name:
                       <br />
-                      Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                      Consequatur sit commodi beatae optio voluptatum sed eius
-                      cumque, delectus saepe repudiandae explicabo nemo nam
-                      libero ad, doloribus, voluptas rem alias. Vitae?
+                      Start Date:
+                      <br />
+                      End Date:
                     </div>
                     <div className="actions">
                       <Popup
