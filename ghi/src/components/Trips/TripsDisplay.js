fb97834277d0a6a8971947fbb9575e1d0846e7e5
Priyanka Kannan <priyankannan91@gmail.com>
2024-02-01 18:00:34 -0600

    Message Adjusted

--- ghi/src/components/Trips/TripsDisplay.js
+++ ghi/src/components/Trips/TripsDisplay.js
@@ -326,11 +326,8 @@ function TripsList(props) {
                         nested
                       >
                         <span>
-                          Lorem ipsum dolor sit amet, consectetur adipisicing
-                          elit. Beatae magni omnis delectus nemo, maxime
-                          molestiae dolorem numquam mollitia, voluptate ea,
-                          accusamus excepturi deleniti ratione sapiente!
-                          Laudantium, aperiam doloribus. Odit, aut.
+                          When successfully submitted replace form modal with
+                          success message text modal.
                         </span>
                       </Popup>
                       <button
